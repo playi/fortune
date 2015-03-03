aecdedfc12f0cbc68112c5051dc2ad1ba26b0721
Dan Chao <dan@play-i.com>
2015-03-03 10:24:59 -0800

    check to make sure FORTUNE_PATH is a real file

--- getFortune.js
+++ getFortune.js
@@ -1,6 +1,14 @@
 var exec = require('child_process').exec;
+var fs   = require('fs');
 
-var fortuneCmd = process.env.FORTUNE_PATH || 'fortune'
+var fortuneCmd = 'fortune';
+
+if (fs.existsSync(process.env.FORTUNE_PATH)) {
+  fortuneCmd = process.env.FORTUNE_PATH;
+} else if (process.env.FORTUNE_PATH) {
+  console.error("couldn't find fortune on path " + process.env.FORTUNE_PATH)
+  process.exit(1)
+}
 
 var getFortune = function(req, res, next){
   exec(fortuneCmd, function(err, stdout, stderr){
